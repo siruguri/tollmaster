47372042efda3a9c786a046cff3a9623395391c9
Sameer Siruguri <siruguri@gmail.com>
2015-07-14 13:33:33 -0700

    Correct migration for Postgres requirements

--- db/migrate/20150713191702_change_door_status_response_column.rb
+++ db/migrate/20150713191702_change_door_status_response_column.rb
@@ -1,9 +1,9 @@
 class ChangeDoorStatusResponseColumn < ActiveRecord::Migration
   def up
-    change_column :door_monitor_records, :door_response, :integer
+    change_column :door_monitor_records, :door_response, 'integer USING CAST(door_response AS integer)'
   end
   
   def down
-    change_column :door_monitor_records, :door_response, :boolean
+    change_column :door_monitor_records, :door_response, 'boolean USING CAST(door_response AS boolean)'
   end
 end
